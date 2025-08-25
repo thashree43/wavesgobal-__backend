42b6764f399fafcd94b670bd03d64fb0aecb1a3c
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2025-08-25 23:21:24 +0530

    ui fi

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://wavesglobal-frontend.vercel.app'], 
-    credentials: true,
-    allowedHeaders: ['Content-Type']
+    origin: '*',
+    allowedHeaders: 'Content-Type',
+    credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
